abd752c3254aaf258e6c5d42cc184bfa6b7f1659
Fabian Strottmann <fabian.strottmann@gmail.com>
2025-12-18 12:25:52 +0100

    ?

--- lib/field/main.dart
+++ lib/field/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 //import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart';
-
 import 'task.dart';
 
 void main() async {
